b46c79133d7b9fd87be1a2fb467a2778dbbfcb6d
pnrlpr <ppinaralper@gmail.com>
2023-06-22 13:48:32 +0300

    total price works

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 
 export const Cart = ({ cart, emptyCart }) => {
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
+
   if (cart.length === 0) return;
   return (
     <div className="border ml-auto w-72 p-4 mt-2 roundedn-lg shadow-lg">
@@ -14,7 +16,7 @@ export const Cart = ({ cart, emptyCart }) => {
         ))}
       </ul>
       <hr className="my-4" />
-      <p className="font-semibold text-xl mt-2">Total: 100$</p>
+      <p className="font-semibold text-xl mt-2">Total: {total} $</p>
       <button
         className="bg-red-500 text-white px-4 py-2 rounded w-full hover:bg-red-600 transition-all mt-4"
         onClick={emptyCart}
